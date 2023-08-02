7b35887eb2794d091d38af922ad92029550a4585
iTzVoko <vokoads@gmail.com>
2023-08-03 00:05:14 +0300

    Add `Simple Multiplication` solution (L1, V1)

---
+++ level-one/video-1/simpleMultiplication.cpp
@@ -0,0 +1,11 @@
+/*
+ * بسم الله الرحمن الرحيم
+ * اللهم صلِّ على محمد وآل محمد
+ *
+ * Solved by: @iTzVoko
+ */
+int simpleMultiplication(int a)
+{
+    // Your code
+    return (a % 2 == 0 ? a * 8 : a * 9);
+}
