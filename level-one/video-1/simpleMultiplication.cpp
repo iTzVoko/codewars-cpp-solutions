f5ba0139acd322c8402d046030f9ccc51c56286f
iTzVoko <vokoads@gmail.com>
2023-08-03 11:14:01 +0300

    Update `simpleMultiplication`: Add explanation comment

--- level-one/video-1/simpleMultiplication.cpp
+++ level-one/video-1/simpleMultiplication.cpp
@@ -3,6 +3,9 @@
  * اللهم صلِّ على محمد وآل محمد
  *
  * Solved by: @iTzVoko
+ * Explanation: a * (8 + a % 2):
+ *              a * (8 + 0 (even)) => a * 8
+ *              a * (8 + 1 (odd)) => a * 9
  */
 int simpleMultiplication(int a)
 {
