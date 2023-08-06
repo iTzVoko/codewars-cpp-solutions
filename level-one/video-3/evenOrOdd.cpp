d083febcc66902055404552e703079ce8d6c9415
iTzVoko <vokoads@gmail.com>
2023-08-06 16:30:01 +0300

    Add `evenOrOdd` solution (L1, V3)

---
+++ level-one/video-3/evenOrOdd.cpp
@@ -0,0 +1,13 @@
+/*
+ * بسم الله الرحمن الرحيم
+ * اللهم صلِّ على محمد وآل محمد
+ *
+ * Solved by: @iTzVoko
+ */
+#include <string>
+
+std::string even_or_odd(int number)
+{
+    return (number % 2 ? "Odd" : "Even");
+    // return (number % 2 == 0 ? "Even" : "Odd");
+}
