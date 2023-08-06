cd67f04560527d9f58e88e68c9904edb125e3c02
iTzVoko <vokoads@gmail.com>
2023-08-06 18:14:54 +0300

    Add `convertABooleanToAString` solution (L1, V3)

---
+++ level-one/video-3/convertABooleanToAString.cpp
@@ -0,0 +1,12 @@
+/*
+ * بسم الله الرحمن الرحيم
+ * اللهم صلِّ على محمد وآل محمد
+ *
+ * Solved by: @iTzVoko
+ */
+#include <string>
+
+std::string boolean_to_string(bool b)
+{
+    return (b ? "true" : "false");
+}
