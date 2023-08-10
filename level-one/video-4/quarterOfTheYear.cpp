bee8a5f660f7e83e753ccab9ed3c24c4eab6d262
iTzVoko <vokoads@gmail.com>
2023-08-10 23:05:27 +0300

    Update `quarterOfTheYear` solution (L1, V4): Use `<=`

--- level-one/video-4/quarterOfTheYear.cpp
+++ level-one/video-4/quarterOfTheYear.cpp
@@ -8,5 +8,5 @@ int quarter_of(int month)
 {
     return (month + 2) / 3;
     // return month / 3.3 + 1;
-    // return (month < 4 ? 1 : (month < 7 ? 2 : (month < 10 ? 3 : 4)));
+    // return (month <= 3 ? 1 : (month <= 6 ? 2 : (month <= 9 ? 3 : 4)));
 }
