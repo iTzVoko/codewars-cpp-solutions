0ac418b7dd4797ac1bb4da6f99b8446b7d91eade
iTzVoko <vokoads@gmail.com>
2023-08-10 23:01:01 +0300

    Add `quarterOfTheYear` solution (L1, V4)

---
+++ level-one/video-4/quarterOfTheYear.cpp
@@ -0,0 +1,12 @@
+/*
+ * بسم الله الرحمن الرحيم
+ * اللهم صلِّ على محمد وآل محمد
+ *
+ * Solved by: @iTzVoko
+ */
+int quarter_of(int month)
+{
+    return (month + 2) / 3;
+    // return month / 3.3 + 1;
+    // return (month < 4 ? 1 : (month < 7 ? 2 : (month < 10 ? 3 : 4)));
+}
