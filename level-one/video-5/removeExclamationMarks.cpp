aa1b5f0a2f4c6bc37cf9183611f6d4e65cedd9b4
iTzVoko <vokoads@gmail.com>
2023-08-16 00:00:10 +0300

    Add `removeExclamationMarks` solution (L1, V5)

---
+++ level-one/video-5/removeExclamationMarks.cpp
@@ -0,0 +1,21 @@
+/*
+ * بسم الله الرحمن الرحيم
+ * اللهم صلِّ على محمد وآل محمد
+ *
+ * Solved by: @iTzVoko
+ */
+#include <string>
+
+std::string removeExclamationMarks(std::string str)
+{
+    int strLength = str.length();
+    std::string result = "";
+
+    for (int i = 0; i < strLength; i++)
+    {
+        if (str[i] != '!')
+            result += str[i];
+    }
+
+    return result;
+}
