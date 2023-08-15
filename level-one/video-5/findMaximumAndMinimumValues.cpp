d32e5fbcfdf7f4f62bb29e8fa3acb74c8f4bb94e
iTzVoko <vokoads@gmail.com>
2023-08-16 00:21:30 +0300

    Add `findMiximumAndMinimumValues` solution (L1, V5) (finish)

---
+++ level-one/video-5/findMaximumAndMinimumValues.cpp
@@ -0,0 +1,37 @@
+/*
+ * بسم الله الرحمن الرحيم
+ * اللهم صلِّ على محمد وآل محمد
+ *
+ * Solved by: @iTzVoko
+ */
+#include <vector>
+
+using namespace std;
+
+int min(vector<int> list)
+{
+    int listSize = list.size();
+    int minimumNumber = list[0];
+
+    for (int i = 0; i < listSize; i++)
+    {
+        if (list[i] < minimumNumber)
+            minimumNumber = list[i];
+    }
+
+    return minimumNumber;
+}
+
+int max(vector<int> list)
+{
+    int listSize = list.size();
+    int maximumNumber = list[0];
+
+    for (int i = 0; i < listSize; i++)
+    {
+        if (list[i] > maximumNumber)
+            maximumNumber = list[i];
+    }
+
+    return maximumNumber;
+}
