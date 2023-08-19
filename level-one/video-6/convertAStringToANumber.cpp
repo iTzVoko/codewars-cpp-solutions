7cfcf3ce1a46b50be9a0d638f462ea12daab8a37
iTzVoko <vokoads@gmail.com>
2023-08-19 11:00:50 +0300

    Update `convertAStringToANumber` solution (L1, V6): Add note
    - `stoi` built-in function can do the job also.

--- level-one/video-6/convertAStringToANumber.cpp
+++ level-one/video-6/convertAStringToANumber.cpp
@@ -4,6 +4,7 @@
  *
  * Solved by: @iTzVoko
  * Note: '0' is a character, and its ASCII value is 48, and '1' is 49, etc.
+ *		 You can use the built-in function "stoi()" (string to integer) too.
  */
 #include <string>
 
@@ -18,7 +19,7 @@ int string_to_number(const std::string &s)
 			continue;
 
 		// ex.     '7' (55) - '0' (48) = 7
-		int digit = s[i] 	- '0';
+		int digit = s[i] - '0';
 
 		result = result * 10 + digit;
 	}
