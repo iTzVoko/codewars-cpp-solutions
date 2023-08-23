2a3ac12b98b2a93ad71d5c3fc1e445bd8ae056d0
iTzVoko <vokoads@gmail.com>
2023-08-23 09:26:22 +0300

    Update `beginnerSeries_2_clock` solution (L1, V8): Add another solution

--- level-one/video-8/beginnerSeries_2_clock.cpp
+++ level-one/video-8/beginnerSeries_2_clock.cpp
@@ -9,5 +9,6 @@ int past(int h, int m, int s)
 
 	return 1000 * (h * 60 * 60 + m * 60 + s);
 	// return ((60 * h + m) * 60 + s) * 1000;
+	// return (h * 3600000) + (60000 * m) + (1000 * s);
 	// return (h * 60 * 60 * 1000) + (m * 60 * 1000) + (s * 1000);
 }
