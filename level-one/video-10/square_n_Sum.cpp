9a9d491228c81ae1d2085d3dc104d13b0cb8df92
iTzVoko <vokoads@gmail.com>
2023-08-24 12:04:39 +0300

    Add `square_n_Sum` solution (L1, V10)

---
+++ level-one/video-10/square_n_Sum.cpp
@@ -0,0 +1,17 @@
+/*
+ * بسم الله الرحمن الرحيم
+ * اللهم صلِّ على محمد وآل محمد
+ *
+ * Solved by: @iTzVoko
+ */
+#include <vector>
+
+int square_sum(const std::vector<int> &numbers)
+{
+	int sum = 0;
+
+	for (int number : numbers)
+		sum += number * number;
+
+	return sum;
+}
