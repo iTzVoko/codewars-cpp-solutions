4cee80330349471182fdc424cfa6f831b0db5224
iTzVoko <vokoads@gmail.com>
2023-08-27 10:10:53 +0300

    Update `whatsTheRealFloor` solution (L1, V11): Add another solution

--- level-one/video-11/whatsTheRealFloor.cpp
+++ level-one/video-11/whatsTheRealFloor.cpp
@@ -3,9 +3,12 @@
  * اللهم صلِّ على محمد وآل محمد
  *
  * Solved by: @iTzVoko
+ * Note: The second solution:
+ * 		 If condetion is true, it will give 1, if it's false, it will give 0
  */
 
 int getRealFloor(int f)
 {
 	return (f <= 0 ? f : (f < 13 ? f - 1 : f - 2));
+	// return f - ((f > 0) + (f > 13));
 }
