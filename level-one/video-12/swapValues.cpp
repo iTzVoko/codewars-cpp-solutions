ac4db60e388fd3d930a25a5047c7696715f4cabb
iTzVoko <vokoads@gmail.com>
2023-08-29 11:46:59 +0300

    Update `swapValues` solution (L1, V12): Without temp value, another 3 ways
    - XOR swap algorithm

--- level-one/video-12/swapValues.cpp
+++ level-one/video-12/swapValues.cpp
@@ -17,20 +17,40 @@ std::pair<int, int> swap_values(std::pair<int, int> values)
 	// values.first  = values.second;
 	// values.second = tempValue;
 
-	// Without temp variable
+	// Without temp variable, let's say that x = 10 and y = 20
 	// Arithmetic way (+ and -)
 	// values.first = values.first + values.second;
 	// values.second = values.first - values.second;
 	// values.first = values.first - values.second;
 
-	// Shorter
+	// * Shorter
 	// values.first += values.second;
 	// values.second = values.first - values.second;
 	// values.first -= values.second;
 
-	// Shortest
+	// * Shortest
 	// values.first += values.second;
 	// values.first -= values.second = values.first - values.second;
+
+	// * XOR Swap Algorithm (from Wikipedia, it's a bit manipulation)
+	// If bits are different (like 1 and 0) it will put 1, if not, it will put 0
+	// 01010 (10) ^ 10100 (20) = 11110 (30) (first)
+	// 11110 (30) ^ 10100 (20) = 01010 (10) (second)
+	// 11110 (30) ^ 01010 (10) = 10100 (20) (first)
+
+	// values.first  = values.first ^ values.second;
+	// values.second = values.first ^ values.second;
+	// values.first  = values.first ^ values.second;
+
+	// * Shorter
+	// values.first ^= values.second;
+	// values.second ^= values.first;
+	// values.first ^= values.second;
+
+	// * Shortest, One-liner solution
+	// values.first ^= values.second ^= values.first ^= values.second;
+
+	// phew... finally :o
 
 	// return values;
 }
