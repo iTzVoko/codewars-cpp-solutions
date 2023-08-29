aceeb160528b7f28888e5e33ecb6dac0acd0d362
iTzVoko <vokoads@gmail.com>
2023-08-29 07:58:10 +0300

    Update `swapValues` solution (L1, V12): Add manual solution

--- level-one/video-12/swapValues.cpp
+++ level-one/video-12/swapValues.cpp
@@ -11,5 +11,9 @@ std::pair<int, int> swap_values(std::pair<int, int> values)
 {
 	std::swap(values.first, values.second);
 
+	// int tempValue = values.first;
+	// values.first = values.second;
+	// values.second = tempValue;
+
 	return values;
 }
