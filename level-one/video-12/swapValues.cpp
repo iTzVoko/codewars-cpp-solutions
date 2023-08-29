83a17ca77f5d1ca0b8891911c02e6ea826edcf52
iTzVoko <vokoads@gmail.com>
2023-08-29 08:23:32 +0300

    Update `swapValues` solution (L1, V12): Without temp value, 3 ways
    - Arithmetic swap algorithm (using + and - only)

--- level-one/video-12/swapValues.cpp
+++ level-one/video-12/swapValues.cpp
@@ -11,9 +11,26 @@ std::pair<int, int> swap_values(std::pair<int, int> values)
 {
 	std::swap(values.first, values.second);
 
+	return values;
+
 	// int tempValue = values.first;
-	// values.first = values.second;
+	// values.first  = values.second;
 	// values.second = tempValue;
 
-	return values;
+	// Without temp variable
+	// Arithmetic way (+ and -)
+	// values.first = values.first + values.second;
+	// values.second = values.first - values.second;
+	// values.first = values.first - values.second;
+
+	// Shorter
+	// values.first += values.second;
+	// values.second = values.first - values.second;
+	// values.first -= values.second;
+
+	// Shortest
+	// values.first += values.second;
+	// values.first -= values.second = values.first - values.second;
+
+	// return values;
 }
