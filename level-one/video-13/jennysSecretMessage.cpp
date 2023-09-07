c123bc8948d82df54fcb3fbc3a3cac37acab0c4c
iTzVoko <vokoads@gmail.com>
2023-09-07 10:01:03 +0300

    Add `JennysSecretMessage` solution (L1, V13)

---
+++ level-one/video-13/jennysSecretMessage.cpp
@@ -0,0 +1,17 @@
+/*
+ * بسم الله الرحمن الرحيم
+ * اللهم صلِّ على محمد وآل محمد
+ *
+ * Solved by: @iTzVoko
+ */
+#include <string>
+
+std::string greet(std::string name)
+{
+	if (name == "Johnny")
+	{
+		return "Hello, my love!";
+	}
+
+	return "Hello, " + name + "!";
+}
