425be53c78fdbd1a220f79be53de8870862b00ca
iTzVoko <vokoads@gmail.com>
2023-09-11 22:20:08 +0300

    Update `rockPaperScissors` solution (L1, V13): Shorten solution

--- level-one/video-13/rockPaperScissors.cpp
+++ level-one/video-13/rockPaperScissors.cpp
@@ -12,27 +12,11 @@ std::string rps(const std::string &p1, const std::string &p2)
 	if (p1 == p2)
 		return "Draw!";
 
-	else if (p1 == "rock")
-	{
-		if (p2 == "paper")
-			return "Player 2 won!";
-		else
-			return "Player 1 won!";
-	}
+	else if (  (p1[0] == 's' && p2[0] == 'p')
+	        || (p1[0] == 'p' && p2[0] == 'r')
+		  	|| (p1[0] == 'r' && p2[0] == 's'))
+		return "Player 1 won!";
 
-	else if (p1 == "paper")
-	{
-		if (p2 == "scissors")
-			return "Player 2 won!";
-		else
-			return "Player 1 won!";
-	}
-
-	else if (p1 == "scissors")
-	{
-		if (p2 == "rock")
-			return "Player 2 won!";
-		else
-			return "Player 1 won!";
-	}
+	else
+		return "Player 2 won!";
 }
