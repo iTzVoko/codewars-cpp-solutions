aeaefe01edf2f78e704776c71f459ba2cb0ac1e5
iTzVoko <vokoads@gmail.com>
2023-09-17 00:04:04 +0300

    Update `abbreviateATwoWordName` solution (L1, V15): Explain

--- level-one/video-15/abbreviateATwoWordName.cpp
+++ level-one/video-15/abbreviateATwoWordName.cpp
@@ -11,8 +11,10 @@ std::string abbrevName(std::string name)
 {
 	std::string result = "";
 
+	// Add the first letter and make it upper case
 	result.push_back(std::toupper(name.front()));
 	result.push_back('.');
+	// Add the letter after the space index and make it upper case
 	result.push_back(std::toupper(name[name.find(' ') + 1]));
 
 	return result;
